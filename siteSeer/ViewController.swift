83e168e883fb6210c1b2340002c6da7f6758079e
Kevin Perkins <kevin.p@ITs-MacBook-Pro-3.local>
2018-03-16 16:05:10 -0400

    setup project with CoreLocation and GamePlayKit

--- siteSeer/ViewController.swift
+++ siteSeer/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 import SpriteKit
 import ARKit
+import CoreLocation
+import GameplayKit
 
-class ViewController: UIViewController, ARSKViewDelegate {
+class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
     
     @IBOutlet var sceneView: ARSKView!
     
@@ -34,7 +38,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = AROrientationTrackingConfiguration()
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -55,11 +59,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
     // MARK: - ARSKViewDelegate
     
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
-        // Create and configure a node for the anchor added to the view's session.
-        let labelNode = SKLabelNode(text: "👾")
-        labelNode.horizontalAlignmentMode = .center
-        labelNode.verticalAlignmentMode = .center
-        return labelNode;
+        return nil
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
